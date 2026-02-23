738be61d68a3dabceb0a99825986d42e42cec8e5
Oksana (Kes) Milenina <OkMilenina@yandex.ru>
2026-02-23 16:23:51 +0300

    ДЗ нз сложные запросы

---
+++ src/main/postgres/orders.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE ORDERS (
+ id SERIAL PRIMARY KEY,
+ date DATE,
+ customer_id INTEGER,
+ product_name VARCHAR(50),
+ amount INTEGER,
+ FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(id)
+);
+
+
